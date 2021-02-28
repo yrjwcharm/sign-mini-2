ac978628adaafd27855f3a2aab5282e1b4821eb0
yrjwcharm <xlyanrui@sina.com>
2021-02-28 22:28:53 +0800

    修复签到bug

--- src/config/api.js
+++ src/config/api.js
@@ -1,20 +1,21 @@
-let wxApi = 'http://175.102.179.84:8090/sign';
+// let wxApi = 'http://175.102.179.84:8090';
+let wxApi = 'https://hstest.youjiankang.net'
 import Constant from '../../project.config.json'
 export default {
     key:'946eedbef5b95fbeff0d6dfb42958db8',
-    login:wxApi+`/user/${Constant.appid}/login`,
-    getImgCode:wxApi+`/user/${Constant.appid}/getImgVerifyCode`,
-    saveUserInfo:wxApi+`/user/${Constant.appid}/saveUserInfo`,
-    saveCompanyInfo:wxApi+`/company/saveCompanyInfo`,
-    saveSignAct:wxApi + `/activity/saveSignActivity`,
-    signList:wxApi + `/sign/getMySignList`,
-    createdAct:wxApi + `/activity/getMyCreate`,
-    getUserInfo:wxApi+`/user/${Constant.appid}/getUserInfo`,
-    deleteAct:wxApi + `/activity/deleteActivity`,
-    getCompany:wxApi+`/company/getCompanyInfo`,
-    updateCompany:wxApi + `/company/updateCompanyInfo`,
-    getActQrWeiMaInfo:wxApi + `/activity/getActivityQRCode`,
-    alreadySignList:wxApi + `/activity/getActivitySignList`,
-    getSignTimes:wxApi + `/sign/getTodaySignInfo`
+    login:wxApi+`/sign/user/${Constant.appid}/login`,
+    getImgCode:wxApi+`/sign/user/${Constant.appid}/getImgVerifyCode`,
+    saveUserInfo:wxApi+`/sign/user/${Constant.appid}/saveUserInfo`,
+    saveCompanyInfo:wxApi+`/sign/company/saveCompanyInfo`,
+    saveSignAct:wxApi + `/sign/activity/saveSignActivity`,
+    signList:wxApi + `/sign/sign/getMySignList`,
+    createdAct:wxApi + `/sign/activity/getMyCreate`,
+    getUserInfo:wxApi+`/sign/user/${Constant.appid}/getUserInfo`,
+    deleteAct:wxApi + `/sign/activity/deleteActivity`,
+    getCompany:wxApi+`/sign/company/getCompanyInfo`,
+    updateCompany:wxApi + `/sign/company/updateCompanyInfo`,
+    getActQrWeiMaInfo:wxApi + `/sign/activity/getActivityQRCode`,
+    alreadySignList:wxApi + `/sign/activity/getActivitySignList`,
+    getSignTimes:wxApi + `/sign/sign/getTodaySignInfo`
 
 }
